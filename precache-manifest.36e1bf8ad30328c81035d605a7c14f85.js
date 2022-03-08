2f61d3bba14f5786eaf2f93dc3a67c74e680cbfb
Muhammad Nurudin <muhammadnurudin01@gmail.com>
2022-03-08 07:19:40 +0700

    deploy

---
+++ precache-manifest.36e1bf8ad30328c81035d605a7c14f85.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "100a199e722281bfd4b0",
+    "url": "/tugas-portofolio/css/app.7e846c92.css"
+  },
+  {
+    "revision": "51d9b81769b65c03bfbc",
+    "url": "/tugas-portofolio/css/chunk-vendors.5d3035bc.css"
+  },
+  {
+    "revision": "c2e076d891e4840d514d875e75df9345",
+    "url": "/tugas-portofolio/img/2833678.c2e076d8.jpg"
+  },
+  {
+    "revision": "34e7180a1b699efa2ad89ac38f383e17",
+    "url": "/tugas-portofolio/img/nutziverse.png"
+  },
+  {
+    "revision": "461c9d9c49d3d5746d38473eba906b51",
+    "url": "/tugas-portofolio/img/umahbali.png"
+  },
+  {
+    "revision": "96293093fa2fe30d27e8acc19c13da7f",
+    "url": "/tugas-portofolio/img/warungmama.png"
+  },
+  {
+    "revision": "e239492626253826210f8a02db29fa20",
+    "url": "/tugas-portofolio/index.html"
+  },
+  {
+    "revision": "1d63b495a317d1c0b3f3",
+    "url": "/tugas-portofolio/js/about.ee51b292.js"
+  },
+  {
+    "revision": "100a199e722281bfd4b0",
+    "url": "/tugas-portofolio/js/app.9c8b6327.js"
+  },
+  {
+    "revision": "51d9b81769b65c03bfbc",
+    "url": "/tugas-portofolio/js/chunk-vendors.ee2b20c8.js"
+  },
+  {
+    "revision": "e97c5160991cd3ef67cb4ebb1761dde9",
+    "url": "/tugas-portofolio/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/tugas-portofolio/robots.txt"
+  }
+]);
